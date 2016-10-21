2614819c32a38b7083bd96d7056c1da32fdd9186
Evan <oakeef@gmail.com>
2016-10-20 22:30:23 -0300

    Added a "questions left" feature.
    The variable questions starts at 3 and the variable questionsLeft
    contians the value of questions and then a string letting the user know
    how many are left. After each question is asked the question variable is
    reduced by one.

--- story.js
+++ story.js
@@ -1,12 +1,27 @@
-var adjective = prompt('Please type an adjective');
-var verb = prompt('Please type a verb');
-var noun = prompt('Please type a noun');
+
+
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left]';
+
+var adjective = prompt('Please type an adjective' + questionsLeft);
+questions -= 1;
+var questionsLeft = ' [' + questions + ' questions left]';
+
+var verb = prompt('Please type a verb' + questionsLeft);
+questions -= 1;
+var questionsLeft = ' [' + questions + ' questions left]';
+
+var noun = prompt('Please type a noun' + questionsLeft);
 var sentence;
 var paragraph;
+
+
 alert('All done. Ready for the message?');
+
 sentence = "<h2>There once was a " + adjective;
 sentence += " warrior who wanted to use his sword to " + verb;
 sentence += " the " + noun + ".</h2>";
+
 paragraph = "<p> The " + adjective + " warrior tried his very best to ";
 paragraph += verb + " the " + noun + " but he just didn't have the power. <br />";
 paragraph += "So the " + adjective +" warrior decided to go to " + noun;
@@ -14,5 +29,7 @@ paragraph += " school to get better at " + verb + "ing.<br />After many years of
 paragraph += noun + " school the " + adjective + " warrior was able to ";
 paragraph += verb + " the " + noun + " and lived happily ever after."
 paragraph += "</p>";
+
+
 document.write(sentence);
 document.write(paragraph);
